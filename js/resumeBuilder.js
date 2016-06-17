b73a994adc4ff0236b42dae1a704a91668e8aade
pedrommbarata <pedrommbarata@gmail.com>
2016-06-17 19:32:09 +0100

    Code cleanup before doing the final changes and encapsulation

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,3 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
 
 var bio = {
     "name": "Pedro Barata",
@@ -32,29 +29,83 @@ jobs: [
    "location": "Carregado, Porutgal",
    "startDate" : 01/10/2012,
    "endDate" : 01/11/2015,
-   "description":"•	Devised effective solutions to complex problems of different areas, delivering time and cost saving, custom built applications to tight deadlines. Lead the IT department team, through several critical projects with great outcome. Thus helping the department, and team, grow and develop, resulting in a 15% increase on response."
+   "description":"Devised effective solutions to complex problems of different areas, delivering time and cost saving, custom built applications to tight deadlines. Lead the IT department team, through several critical projects with great outcome. Thus helping the department, and team, grow and develop, resulting in a 15% increase on response."
 }
 ]
 };
+
+work.display = function()
+{
+    if(work.jobs.length>0)
+    {
+        for (var job in work.jobs) {
+            if ( work.jobs.hasOwnProperty(job)) {
+                var element = work.jobs[job];
+               
+                $("#workExperience").append(HTMLworkStart);
+                var formattedEmployer = HTMLworkEmployer.replace("%data%",element.employer);
+                
+                $(".work-entry:last").append(formattedEmployer + formattedTitle);
+                var formattedTitle = HTMLworkTitle.replace("%data%",element.title);
+                
+                $(".work-entry:last").append(formattedDate);
+                var formattedDate = HTMLworkDates.replace("%data%",element.startDate);
+              
+                var formattedLocation = HTMLworkLocation.replace("%data%",element.location);
+                $(".work-entry:last").append(formattedLocation);
+                var formattedDescription = HTMLworkDescription.replace("%data%",element.description);
+                $(".work-entry:last").append(formattedDescription);
+                
+            }
+        }
+    }
+}
+
+
 
 var projects = {
     projects: [
         {
             "title": "Picking System",
-            "startDate":01/01/2015,
-            "endDate":30/10/2015,
-            "descrption":"Control of the entire shipping process. The control of the shipping process was non-existent and the projected responded to a need from the end user, optimizing the logistics' time, security and eliminating penalties.",
+            "dates":"01/2015 - 10/2015",
+            "description":"Control of the entire shipping process. The control of the shipping process was non-existent and the projected responded to a need from the end user, optimizing the logistics' time, security and eliminating penalties.",
             "images": ""
         },
         {
-            "title": "EITRI – ERP para Tooling",
-            "startDate":01/06/2013,
-            "endDate":31/04/2014,
+            "title": "EITRI – ERP for Tooling",
+            "dates":"06/2013 - 04/2014",
             "description": "Functional analysis, conception and development of a custom built ERP solution, controlling the entire manufacturing process. This led to a 50% improvement in throughput time and to a 75% reduction of manufacturing costs.",
             "images": ""
         }
     ]
 };
+
+projects.display = function()
+{
+    if(projects.projects.length > 0)
+    {
+        for (var prj in projects.projects) {
+                if ( projects.projects.hasOwnProperty(prj)) {
+                    var project = projects.projects[prj];
+                    
+                    $("#projects").append(HTMLprojectStart);
+                    
+                    var formattedTitle = HTMLprojectTitle.replace("%data%",project.title);
+                    $(".project-entry:last").append(formattedTitle );
+                    
+                    var formattedDates = HTMLprojectTitle.replace("%data%",project.dates);
+                    $(".project-entry:last").append(formattedDates );
+                    
+                    var formattedDescription = HTMLprojectDescription.replace("%data%",project.description);
+                    $(".project-entry:last").append(formattedDescription );
+                    
+                    var formattedImages = HTMLprojectImage.replace("%data%",project.images);
+                    $(".project-entry:last").append(formattedImages );
+            }
+        }
+    }
+}
+
 
 var education = {
     "schools": [
@@ -81,3 +132,27 @@ var education = {
     ]
 };
 
+var formattedHeaderName = HTMLheaderName.replace("%data%",bio.name);
+var formatedHeaderRole = HTMLheaderRole.replace("%data%",bio.role);
+$("#header").append(formattedHeaderName + formatedHeaderRole);
+
+if(bio.skills.length>0){
+    $("#header").append(HTMLskillsStart);
+    
+    var formatedSkill = "";
+    for (i = 0; i < bio.skills.length; i++) { 
+        formatedSkill = HTMLskills.replace("%data%",bio.skills[i]);
+         $("#skills").append(formatedSkill);
+    }
+}
+
+
+
+
+
+
+//Call the display methods for all the objects
+$("#main").append(internationalizeButton);
+displayWork();
+projects.display();
+$("#mapDiv"). append(googleMap);
