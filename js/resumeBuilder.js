6ccf7fe1a56131e0f41a363e2fcc4f1aaf6a302e
pedrommbarata <pedrommbarata@gmail.com>
2016-06-17 09:39:49 +0100

    <<<<<<<<<<<<<<<<<<Finished lesson about DataTypes.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,83 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-$("#main").append("Pedro Barata");
+
+var bio = {
+    "name": "Pedro Barata",
+    "role": "Application Developer",
+    "welcomeMessage":"I have a passion for acquiring new knowledge and I am able to quickly understand complex problems and devise effective solutions. This allows me to efficiently work with teams and organizations, helping them to overcome challenges by developing tools which add business value.",
+    "contacts": {
+        "mobile":  "+351 919171624" ,
+        "email": "pedrommbarata@gmail.com",
+        "github":"pedrommbarata",
+        "twitter":"pedrommbarata",
+        "location":"Lisbon, Portugal"
+    },
+    "skills":["C#", ".NET","SQL", "T-SQL", "WCF","HTML","Javascript"]
+};
+
+var work= {
+jobs: [ 
+{
+   "employer": "MCG – Mind for Metal" ,
+   "title": "Planning and Production Control Manager",
+   "location": "Carregado, Porutgal",
+   "startDate" : 01/11/2015,
+   "endDate" : 10/06/2016,
+   "description":"Leading the production control team to ensure deadlines are met and devise new methods to continuously improve planning and production. Data analysis of production process bottlenecks to ensure a fine tune of the planning done at these critical points, increasing their efficiency."
+},
+{
+   "employer": "MCG – Mind for Metal" ,
+   "title": "Application Developer, Data Analyst",
+   "location": "Carregado, Porutgal",
+   "startDate" : 01/10/2012,
+   "endDate" : 01/11/2015,
+   "description":"•	Devised effective solutions to complex problems of different areas, delivering time and cost saving, custom built applications to tight deadlines. Lead the IT department team, through several critical projects with great outcome. Thus helping the department, and team, grow and develop, resulting in a 15% increase on response."
+}
+]
+};
+
+var projects = {
+    projects: [
+        {
+            "title": "Picking System",
+            "startDate":01/01/2015,
+            "endDate":30/10/2015,
+            "descrption":"Control of the entire shipping process. The control of the shipping process was non-existent and the projected responded to a need from the end user, optimizing the logistics' time, security and eliminating penalties.",
+            "images": ""
+        },
+        {
+            "title": "EITRI – ERP para Tooling",
+            "startDate":01/06/2013,
+            "endDate":31/04/2014,
+            "description": "Functional analysis, conception and development of a custom built ERP solution, controlling the entire manufacturing process. This led to a 50% improvement in throughput time and to a 75% reduction of manufacturing costs.",
+            "images": ""
+        }
+    ]
+};
+
+var education = {
+    "schools": [
+    {
+        "name":"FCT-UNL",
+        "location":"Monte da Caparica, Portugal",
+        "degree": "MsC in Biomedical Engineering",
+        "url":"http://www.fct.unl.pt/en"
+    },
+    {
+        "name":"FCT-UNL",
+        "location":"Monte da Caparica, Portugal",
+        "degree": "BsC in Biomedical Engineering",
+        "url":"http://www.fct.unl.pt/en"
+    }
+    ],
+    "onlineCourses" : [
+        {
+            "title": "Javascript Basics",
+            "school": "Udacity",
+            "startDate":"",
+            "endDate":""
+        }
+    ]
+};
+
